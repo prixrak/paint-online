d750e4348bdd477b25fd48ed13f697009e55ef0f
prixrak <20arman15@gmail.com>
2022-01-15 23:13:03 +0200

    add circle and line drawing

--- client/src/components/Toolbar.jsx
+++ client/src/components/Toolbar.jsx
@@ -4,14 +4,16 @@ import toolState from '../store/toolState';
 import '../styles/toolbar.scss'
 import Brush from './../tools/Brush';
 import Rect from './../tools/Rect';
+import Circle from './../tools/Circle';
+import Line from './../tools/Line';
 const Toolbar = () => {
   return (
     <div className='toolbar'>
       <i className="fas fa-paint-brush" onClick={() => toolState.setTool(new Brush(canvasState.canvas))}></i>
       <i className="fas fa-square" onClick={() => toolState.setTool(new Rect(canvasState.canvas))}></i>
-      <i className="fas fa-circle"></i>
+      <i className="fas fa-circle" onClick={() => toolState.setTool(new Circle(canvasState.canvas))}></i>
       <i className="fas fa-eraser"></i>
-      <i className="fab fa-line"></i>
+      <i className="fab fa-line" onClick={() => toolState.setTool(new Line(canvasState.canvas))}></i>
       <label>
         <i className="fas fa-palette"></i>
         <input type="color" />
