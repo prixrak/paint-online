f58d865296943b5ce3ffdec7dbf14f09d4e1ee69
prixrak <20arman15@gmail.com>
2022-01-15 18:38:06 +0200

    add drawing rect

--- client/src/components/Toolbar.jsx
+++ client/src/components/Toolbar.jsx
@@ -3,11 +3,12 @@ import canvasState from '../store/canvasState';
 import toolState from '../store/toolState';
 import '../styles/toolbar.scss'
 import Brush from './../tools/Brush';
+import Rect from './../tools/Rect';
 const Toolbar = () => {
   return (
     <div className='toolbar'>
       <i className="fas fa-paint-brush" onClick={() => toolState.setTool(new Brush(canvasState.canvas))}></i>
-      <i className="fas fa-square"></i>
+      <i className="fas fa-square" onClick={() => toolState.setTool(new Rect(canvasState.canvas))}></i>
       <i className="fas fa-circle"></i>
       <i className="fas fa-eraser"></i>
       <i className="fab fa-line"></i>
--- client/src/store/toolState.js
+++ client/src/store/toolState.js
@@ -2,6 +2,7 @@ import { makeAutoObservable } from "mobx";
 
 class ToolState {
   tool = null;
+  
   constructor() {
     makeAutoObservable(this);
   }
